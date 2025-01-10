03d40f6bfd5e6dff9cd4b4b85a2270e62fe498f1
Bekir Murtazaev <bekirmurtazaev@yandex.ru>
2025-01-10 06:36:33 +0300

    update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,21 @@
+'use client'
+import { useState } from 'react'
+import CustomForm from '@/app/components/CustomForm/CustomForm'
+import EventCardList from '@/app/components/EventCardList/EventCardList'
+import { IEvent, Events } from '@/app/types/types'
+
 export default function Home() {
-  return <div>Hello</div>
+  const [events, setEvents] = useState<Events>([])
+  const createNewEvent = (newEvent: IEvent): void => {
+    const eventsCopy: IEvent[] = events.slice()
+    eventsCopy.push(newEvent)
+    setEvents(eventsCopy)
+  }
+  
+  return (
+    <div>
+      <CustomForm submitHandler={createNewEvent} />
+      <EventCardList events={events} />
+    </div>
+  )
 }
