5871a5022d559d4c49a8551f749add9ddec610b2
Bekir Murtazaev <bekirmurtazaev@yandex.ru>
2025-01-24 23:37:30 +0300

    add deleteEvent method into EventCard component

--- src/components/EventCardList/EventCard/EventCard.tsx
+++ src/components/EventCardList/EventCard/EventCard.tsx
@@ -1,16 +1,23 @@
 import Style from './EventCard.module.css'
 import { IEvent } from '@/types/types'
+import { deleteEvent } from '@/redux/slices/eventsSlice'
+import { useDispatch } from 'react-redux'
 
 export default function EventCard({
+  id,
   image,
   title,
   description,
   location,
   createdAt,
 }: IEvent) {
+  const dispatch = useDispatch()
   const getFormattedEventDate = () => {
     const eventDate = new Date(JSON.parse(createdAt))
     return eventDate.toLocaleString('ru-Ru', { timeZone: 'UTC' }).split(', ')[0]
+  }
+  const handleDeleteEvent = () => {
+    dispatch(deleteEvent(id))
   }
 
   return (
@@ -25,7 +32,7 @@ export default function EventCard({
       <p>Location: {location}</p>
       <p>Created at: {getFormattedEventDate()}</p>
       <div className={Style.buttons}>
-        <button>Delete</button>
+        <button onClick={handleDeleteEvent}>Delete</button>
         <button>Edit</button>
       </div>
     </div>
