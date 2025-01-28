af866171f5987adef476547524cc232aeab44632
Bekir Murtazaev <bekirmurtazaev@yandex.ru>
2025-01-29 00:32:30 +0300

    use Popup component into EventCardList

--- src/components/EventCardList/EventCardList.tsx
+++ src/components/EventCardList/EventCardList.tsx
@@ -3,28 +3,29 @@ import EventCard from '@/components/EventCardList/EventCard/EventCard'
 import { useSelector } from 'react-redux'
 import { RootState } from '@/redux/store'
 import { IEvent } from '@/types/types'
+import Popup from '@/components/Popup/Popup'
 
 export default function EventCardList() {
   const { events } = useSelector((state: RootState) => state.events)
+  const {title, variant} = useSelector((state: RootState) => state.popup.info)
 
   return (
     <div className={Style.eventList}>
       {events.length >= 1 && (
         <div>
-          {events.map((event: IEvent, index: number) => (
+          {events.map((event: IEvent) => (
             <EventCard
-              id={event.id}
-              image={event.image}
-              title={event.title}
-              description={event.description}
-              location={event.location}
-              createdAt={event.createdAt}
-              key={index}
+              eventData={event}
+              key={event.id}
             />
           ))}
         </div>
       )}
       {events.length === 0 && <div>No events</div>}
+      <Popup
+        title={title}
+        variant={variant}
+      />
     </div>
   )
 }
