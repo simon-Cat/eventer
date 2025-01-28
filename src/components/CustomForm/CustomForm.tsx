1f0e70d4969e58a8d5f482fc0e46efcec40c8241
Bekir Murtazaev <bekirmurtazaev@yandex.ru>
2025-01-29 00:35:13 +0300

    add props, add submitFormEdit, use popup method

--- src/components/CustomForm/CustomForm.tsx
+++ src/components/CustomForm/CustomForm.tsx
@@ -3,9 +3,10 @@ import { useForm, SubmitHandler } from 'react-hook-form'
 import { IEvent } from '@/types/types'
 import { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
-import { addEvent } from '@/redux/slices/eventsSlice'
+import { addEvent, updateEvent } from '@/redux/slices/eventsSlice'
+import { closeAllPopups } from '@/redux/slices/popupSlice'
 
-export default function CustomForm() {
+export default function CustomForm({eventData}: {eventData?: IEvent}) {
   const dispatch = useDispatch()
   const {
     register,
@@ -19,7 +20,22 @@ export default function CustomForm() {
     const randomId = +(Math.random() * 100).toFixed()
     const eventData: IEvent = { ...formData, createdAt: createdDate, id: randomId }
 
+    dispatch(closeAllPopups())
     dispatch(addEvent(eventData))
+  }
+
+  const submitFormEdit: SubmitHandler<IEvent> = (newEventData) => {
+    if(eventData) {
+      const updateDate = JSON.stringify(new Date())
+      
+      dispatch(closeAllPopups())
+      dispatch(updateEvent({
+        ...newEventData,
+        id: eventData.id,
+        createdAt: eventData.createdAt,
+        updateAt: updateDate
+      }))
+    }
   }
 
   useEffect(() => {
@@ -30,33 +46,38 @@ export default function CustomForm() {
 
   return (
     <form
-      onSubmit={handleSubmit(submitForm)}
+      onSubmit={handleSubmit(eventData ? submitFormEdit : submitForm)}
       className={Style.form}
     >
       <input
         type="url"
+        defaultValue={eventData ? eventData.image : ''}
         {...register('image', { required: true })}
       />
       {errors.image && <span className={Style.error}>Required insert image URL</span>}
 
       <input
         type="text"
+        defaultValue={eventData ? eventData.title : ''}
         {...register('title', { required: true })}
       />
       {errors.title && <span className={Style.error}>Required insert title</span>}
 
       <textarea
+        defaultValue={eventData ? eventData.description : ''}
         {...register('description', { required: true })}
+        
       />
       {errors.description && <span className={Style.error}>Required insert description</span>}
 
       <input
         type="text"
+        defaultValue={eventData ? eventData.location : ''}
         {...register('location', { required: true })}
       />
       {errors.location && <span className={Style.error}>Required insert location</span>}
 
-      <button type="submit">Создать</button>
+      <button type="submit">{eventData ? 'Сохранить изменения' : 'Создать'}</button>
     </form>
   )
 }
